32e8b96f5ec6e76a64b479e3d4f49636536b2d0c
Tommaso <tommaso-meledina@users.noreply.github.com>
2025-09-10 14:00:19 +0200

    feat: add outbound proxy capability

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,13 @@ RUN apt-get update && apt-get install -y \
     iproute2 \
     iputils-ping \
     curl \
+    python3 \
+    python3-pip \
     && rm -rf /var/lib/apt/lists/*
+
+RUN pip3 install px-proxy
+
+ENV PATH="/usr/local/bin:${PATH}"
 
 COPY squid.conf /etc/squid/squid.conf.template
 
--- start.sh
+++ start.sh
@@ -41,8 +41,35 @@ sleep $WAIT_TIME_SECONDS
 log "...done waiting, starting squid"
 
 SQUID_PORT=${SQUID_PORT:-3128}
+PX_PORT=${PX_PORT:-3129}
+PX_HOST=${PX_HOST:-127.0.0.1}
+
 sed "s/{{SQUID_PORT}}/${SQUID_PORT}/g" /etc/squid/squid.conf.template > /etc/squid/squid.conf
+
+if [ -z "$OUTBOUND_PROXY_PAC_URL" ]; then
+  log "No OUTBOUND_PROXY_PAC_URL specified, not amending squid configuration"
+else
+  log "OUTBOUND_PROXY_PAC_URL specified, amending squid configuration"
+  echo "" >> /etc/squid/squid.conf
+  echo "cache_peer 127.0.0.1 parent $PX_PORT 0 no-query default" >> /etc/squid/squid.conf
+  echo "" >> /etc/squid/squid.conf
+  echo "never_direct allow all" >> /etc/squid/squid.conf
+  echo "" >> /etc/squid/squid.conf
+fi
 
 log "About to start squid on port $SQUID_PORT"
 
-squid -N -d1
+squid -N -d1 &
+
+log "squid kicked off"
+
+if [ -z "$OUTBOUND_PROXY_PAC_URL" ]; then
+  log "No OUTBOUND_PROXY_PAC_URL specified, skipping px-proxy startup"
+else
+  log "waiting $WAIT_TIME_SECONDS seconds..."
+  sleep $WAIT_TIME_SECONDS
+  log "...done waiting, starting px-proxy with PAC location $OUTBOUND_PROXY_PAC_URL on port $PX_PORT"
+  px --pac="$OUTBOUND_PROXY_PAC_URL" --listen"$PX_HOST" --port="$PX_PORT" --debug &
+fi
+
+wait -n
